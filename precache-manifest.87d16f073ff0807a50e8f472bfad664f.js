fa3ede43db7896c98b6bb30647204841c8acbfec
Ysraelnm <ysraelnm@outlook.com>
2019-05-23 15:13:47 -0500

    Proyecto finalizado V1

---
+++ precache-manifest.87d16f073ff0807a50e8f472bfad664f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880305b22d82267c2875a7bb3c928ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d7703516369a27f886",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "68d7703516369a27f886",
+    "url": "/static/js/main.255eb2fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
